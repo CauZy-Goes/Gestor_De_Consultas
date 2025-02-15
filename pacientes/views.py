b358500ae853a3135e87f3848782976ca9fc27b3
CauZy-Goes <cauafariasdev@gmail.com>
2025-02-15 16:19:46 -0300

    feat: add consulta ands its features

--- pacientes/views.py
+++ pacientes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Pacientes
-from django.http import HttpResponse
+from .models import Pacientes, Tarefas, Consultas
+from django.http import HttpResponse, Http404
 from django.contrib import messages
 from django.contrib.messages import constants
 
@@ -35,7 +35,35 @@ def pacientes(request):
 def paciente_view(request, id):
     paciente = Pacientes.objects.get(id=id)
     if request.method == "GET":
-        return render(request, 'paciente.html', {'paciente': paciente})
+        tarefas = Tarefas.objects.all()
+        consultas = Consultas.objects.filter(paciente=paciente)
+
+        tuple_grafico = ([str(i.data) for i in consultas], [str(i.humor) for i in consultas])
+
+        return render(request, 'paciente.html', {'tarefas': tarefas, 'paciente': paciente, 'consultas': consultas, 'tuple_grafico': tuple_grafico})
+    else:
+        humor = request.POST.get('humor')
+        registro_geral = request.POST.get('registro_geral')
+        video = request.FILES.get('video')
+        tarefas = request.POST.getlist('tarefas')
+
+        consultas = Consultas(
+            humor=int(humor),
+            registro_geral=registro_geral,
+            video=video,
+            paciente=paciente
+        )
+        consultas.save()
+
+        for i in tarefas:
+            tarefa = Tarefas.objects.get(id=i)
+            consultas.tarefas.add(tarefa)
+
+        consultas.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Registro de consulta adicionado com sucesso.')
+        return redirect(f'/pacientes/{id}')
+
     
 def atualizar_paciente(request, id):
     paciente = Pacientes.objects.get(id=id)
@@ -44,3 +72,15 @@ def atualizar_paciente(request, id):
     paciente.pagamento_em_dia = status
     paciente.save()
     return redirect(f'/pacientes/{id}')
+
+def excluir_consulta(request, id):
+    consulta = Consultas.objects.get(id=id)
+    consulta.delete()
+    return redirect(f'/pacientes/{consulta.paciente.id}')
+
+def consulta_publica(request, id):
+    consulta = Consultas.objects.get(id=id)
+    if not consulta.paciente.pagamento_em_dia:
+        raise Http404()
+
+    return render(request, 'consulta_publica.html', {'consulta': consulta})
